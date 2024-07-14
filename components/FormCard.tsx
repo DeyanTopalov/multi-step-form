eb98168ef917d08f6ddae5ceded8ed97764e415f
DeyanTopalov <deyan.topalow@gmail.com>
2024-07-15 00:49:18 +0300

    steps validation logic

--- components/FormCard.tsx
+++ components/FormCard.tsx
@@ -3,38 +3,44 @@
 import { useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TTestSchema, testSchema } from "@lib/schema";
 
+const validFields: Array<keyof TTestSchema> = ["name", "email", "phoneNumber"];
 const steps = [
   {
     id: "Step 1",
     name: "Your info",
     title: "Personal info",
     description: "Please provide your name, email address, and phone number.",
-    fields: ["name", "email", "phoneNumber"],
+    fields: ["name", "email", "phoneNumber"] as const,
   },
   {
     id: "Step 2",
     name: "Select plan",
     title: "Select your plan",
     description: "You have the option of monthly or yearly billing.",
-    fields: ["Arcade", "Advanced", "Pro"],
+    fields: ["Arcade", "Advanced", "Pro"] as const,
   },
   {
     id: "Step 3",
     name: "Add-ons",
     title: "Pick add-ons",
     description: "Add-ons help enhance your gaming experience.",
-    fields: ["Online service", "Larger storage", "Customizable profile"],
+    fields: [
+      "Online service",
+      "Larger storage",
+      "Customizable profile",
+    ] as const,
   },
   {
     id: "Step 4",
     name: "Summary",
     title: "Finishing up",
     description: "Double-check everything looks OK before confirming.",
-    fields: [],
+    fields: [] as const,
   },
 ];
 
@@ -68,6 +74,7 @@ export const FormNav = ({ currentStep }: { currentStep: number }) => {
 export const FormCard = () => {
   const {
     register,
+    trigger,
     handleSubmit,
     formState: { errors, isSubmitting },
     reset,
@@ -81,24 +88,43 @@ export const FormCard = () => {
   console.log("current step is: ", currentStep);
   console.log("steps are: ", steps.length);
 
-  const handleNextStep = () => {
+  const handleNextStep = async () => {
     if (currentStep === steps.length) {
       return;
     }
-    setCurrentStep(currentStep + 1);
+
+    const currentFields = steps[currentStep].fields;
+    const fieldsToValidate = currentFields.filter((field) =>
+      validFields.includes(field as keyof TTestSchema),
+    ) as Array<keyof TTestSchema>;
+
+    const isStepValid = await trigger(fieldsToValidate);
+
+    if (isStepValid) {
+      console.log("Validation passed. Moving to the next step.");
+      setCurrentStep((prevStep) => prevStep + 1);
+    } else {
+      console.log("Validation failed. Staying on the current step.");
+      console.log(errors);
+    }
   };
 
   const handlePrevStep = () => {
     if (currentStep === 1) {
       return;
     }
-    setCurrentStep(currentStep - 1);
+    setCurrentStep((prevStep) => prevStep - 1);
+  };
+
+  const onSubmit = (data: TTestSchema) => {
+    console.log(data);
+    reset();
   };
 
   return (
     <div className="grid grid-cols-2 gap-2">
       <FormNav currentStep={currentStep} />
-      <form className="grid gap-4">
+      <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
         {/* Step 1 */}
         {currentStep === 1 && (
           <section>
@@ -106,7 +132,22 @@ export const FormCard = () => {
               <Label htmlFor="name" className="text-base font-normal">
                 Name
               </Label>
-              <Input id="name" type="text" placeholder="e.g. Stephen King" />
+              <Input
+                id="name"
+                type="text"
+                placeholder="e.g. Stephen King"
+                {...register("name")}
+                aria-describedby="name-error"
+              />
+              <div
+                id="name-error"
+                aria-live="polite"
+                className="flex h-6 w-full items-center justify-start"
+              >
+                {errors.name && (
+                  <p className="text-xs text-red-500">{errors.name.message}</p>
+                )}
+              </div>
             </div>
           </section>
         )}
@@ -122,7 +163,18 @@ export const FormCard = () => {
                 id="email"
                 type="text"
                 placeholder="e.g. stephenking@lorem.com"
+                {...register("email")}
+                aria-describedby="email-error"
               />
+              <div
+                id="email-error"
+                aria-live="polite"
+                className="flex h-6 w-full items-center justify-start"
+              >
+                {errors.email && (
+                  <p className="text-xs text-red-500">{errors.email.message}</p>
+                )}
+              </div>
             </div>
           </section>
         )}
@@ -138,15 +190,34 @@ export const FormCard = () => {
                 id="phoneNumber"
                 type="number"
                 placeholder="e.g. +1 234 567 890"
+                {...register("phoneNumber")}
+                aria-describedby="phoneNumber-error"
               />
+              <div
+                id="phoneNumber-error"
+                aria-live="polite"
+                className="flex h-6 w-full items-center justify-start"
+              >
+                {errors.phoneNumber && (
+                  <p className="text-xs text-red-500">
+                    {errors.phoneNumber.message}
+                  </p>
+                )}
+              </div>
             </div>
           </section>
         )}
+        <Button
+          type="submit"
+          className="hover:bg-grey-900 w-full cursor-pointer bg-green-600 font-bold"
+          disabled={isSubmitting}
+        >
+          Submit
+        </Button>
       </form>
       <div className="mt-4 flex items-center gap-4">
         <button
           onClick={handlePrevStep}
-          //   className={` ${currentStep === 1 ? "bg-red-400" : "bg-blue-400"} rounded-lg px-4 py-2`}
           className="rounded-lg bg-blue-400 px-4 py-2 disabled:bg-red-400"
           disabled={currentStep === 1}
         >
@@ -163,3 +234,44 @@ export const FormCard = () => {
     </div>
   );
 };
+
+//! Backup logic
+// const handleNextStep = async () => {
+//   if (currentStep === steps.length) {
+//     return;
+//   }
+
+//   if (currentStep === 1) {
+//     const isStepValid = await trigger("name");
+
+//     if (isStepValid) {
+//       console.log("Validation passed. Moving to the next step.");
+//       setCurrentStep((prevStep) => prevStep + 1);
+//     } else {
+//       console.log("Validation failed. Staying on the current step.");
+//       console.log(errors);
+//     }
+//   }
+//   if (currentStep === 2) {
+//     const isStepValid = await trigger("email");
+
+//     if (isStepValid) {
+//       console.log("Validation passed. Moving to the next step.");
+//       setCurrentStep((prevStep) => prevStep + 1);
+//     } else {
+//       console.log("Validation failed. Staying on the current step.");
+//       console.log(errors);
+//     }
+//   }
+//   if (currentStep === 3) {
+//     const isStepValid = await trigger("phoneNumber");
+
+//     if (isStepValid) {
+//       console.log("Validation passed. Moving to the next step.");
+//       setCurrentStep((prevStep) => prevStep + 1);
+//     } else {
+//       console.log("Validation failed. Staying on the current step.");
+//       console.log(errors);
+//     }
+//   }
+// };
